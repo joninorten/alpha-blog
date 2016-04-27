ca2397afcc33eec60265aad6054905eeed53e06f
Joni <joni.one.eye@gmail.com>
2016-04-27 08:48:50 +0000

    update current user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -57,13 +57,13 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
-  
+
     def require_same_user
       if current_user != @article.user
         flash[:danger] = "You can only edit or delete your own articles"
         redirect_to root_path
       end
     end
-    
+
 
 end
